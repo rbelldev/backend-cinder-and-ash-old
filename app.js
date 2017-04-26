0ef77c0fa058504f972096eb70fa7bfc11021121
Rob Bell <rbelldev@gmail.com>
2017-04-25 23:32:02 -0400

    Removed 404 forwarder

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ app.post("/application", function (req, res) {
     // });
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
